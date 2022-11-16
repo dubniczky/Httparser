d4b16b7444414be5ef05e33d6e9c1577e0ea1ca7
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-11-16 23:34:25 +0100

    Added basic jest test

--- test.js
+++ test.js
@@ -1,9 +1,15 @@
 import fs from 'fs'
+import { describe, test } from '@jest/globals'
+
 import HTTPRequest from './http-request.js'
 import HTTPResponse from './http-response.js'
 
-const data = fs.readFileSync('examples/get.request.http', 'utf8')
-console.dir(new HTTPRequest(data), { depth: null })
 
-const data2 = fs.readFileSync('examples/get.response.http', 'utf8')
-console.dir(new HTTPResponse(data2), { depth: null })
+describe("HTTP Request", () => {
+    test("Parse HTTP Request", () => {
+        const data = fs.readFileSync('examples/get.request.http', 'utf8')
+        const request = new HTTPRequest(data)
+        expect(request.method).toBe('GET')
+        expect(request.url).toBe('/index.html')
+    })
+})
